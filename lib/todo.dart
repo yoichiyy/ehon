d12d920a1a0de9762025aafcc33fd00fdf2af9a4
yoichiyy <yoichiyy@gmail.com>
2022-07-30 07:28:35 +0900

    大きいパソで、editとtimestamp

--- lib/todo.dart
+++ lib/todo.dart
@@ -5,17 +5,23 @@ class Todo {
   String dueDate = "";
   String dueTime = "";
   String id = "";
+  String timestamp = "";
 
   //コンストラクタ
   Todo(DocumentSnapshot documentSnapshot) {
     //変数名から型がわかるように（複数、単数を参考に）
     taskName = (documentSnapshot.data() as Map<String, dynamic>)["title"];
 
+//F質問：このtimestampそのものを、編集することはできないのか？もしだめなら、
+//1.timestampでなくて、年月日時でfirebase側も管理する。タスク登録時点で、この４つを送信する。
+//2.edit画面のコントローラーを工夫？して、timestampを表示/編集できるcontrollerにする。
     final Timestamp timestamp =
         (documentSnapshot.data() as Map<String, dynamic>)['createdAt'];
 
-    dueDate = '${timestamp.toDate().month.toString()} - ${timestamp.toDate().day.toString()}';
-    dueTime = '${timestamp.toDate().hour.toString()} - ${timestamp.toDate().minute.toString()}';
+    dueDate =
+        '${timestamp.toDate().month.toString()} - ${timestamp.toDate().day.toString()}';
+    dueTime =
+        '${timestamp.toDate().hour.toString()} - ${timestamp.toDate().minute.toString()}';
     id = documentSnapshot.id;
   }
 }
