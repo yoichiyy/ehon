b78782bea6cccb32b413fa16089f4c85d758cd85
yoichiyy <yoichiyy@gmail.com>
2022-08-03 06:43:03 +0900

    803朝統合

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -32,6 +32,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: const Text("ehon"),
       ),
